dc3635fbd62dd694ef5d4ba222afe10afab3ab98
victor <victor.njutha@student.moringaschool.com>
2023-10-20 12:09:49 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+li.textContent = "Hi there!";
+// => <li>Hi there!</li>
+console.log(li.textContent);
+// => "Hi there!"
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+  element.textContent = "You've changed what's on the screen!";
+  element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+element.className = "pet-listing dog";
